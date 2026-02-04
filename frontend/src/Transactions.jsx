45121b0944a5ef48f358ba64ac48f0dce89afaab
Rivka <instech101@gmail.com>
2026-02-04 13:02:35 -0500

    do after 4 seconds to wait for it to show up in our system

--- frontend/src/Transactions.jsx
+++ frontend/src/Transactions.jsx
@@ -641,8 +641,10 @@ function Transactions({ user }) {
               <PaymentForm
                 isPortal={true}
                 onSuccess={() => {
-                  setShowNewTransScreen(false);
-                  search(getFilters());
+                  setTimeout(() => {
+                    setShowNewTransScreen(false);
+                    search(getFilters());
+                  }, 4000)
                 }}
               />
             </div>
@@ -707,18 +709,18 @@ function Transactions({ user }) {
           </div>
 
           <div className="header-right">
-            { user?.Role?.toLowerCase() != "viewer" &&
-              
+            {user?.Role?.toLowerCase() != "viewer" &&
+
               <button
-              className="btn-new-tx"
-              type="button"
-              onClick={() => setShowNewTransScreen(true)}
-            >
-              <svg width="16" height="16" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-              </svg>
-              New Transaction
-            </button>
+                className="btn-new-tx"
+                type="button"
+                onClick={() => setShowNewTransScreen(true)}
+              >
+                <svg width="16" height="16" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+                </svg>
+                New Transaction
+              </button>
             }
 
             <div className="secondary-actions">
