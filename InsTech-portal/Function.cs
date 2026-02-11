de384052bf540ea183a274ae69f73f2eec9dd6a9
Rivka <instech101@gmail.com>
2026-02-11 11:28:26 -0500

    hide users even for hidden users

--- InsTech-portal/Function.cs
+++ InsTech-portal/Function.cs
@@ -664,8 +664,9 @@ public class Function
                 if (user?.FirstOrDefault(u => u.VendorId == vendor.Id)?.Role == "admin")
                 {
                     var users = await InsTechClassesV2.User.GetUsersAsync(vendor.Id);
-                    bool HideUser = user?.FirstOrDefault(u => u.VendorId == vendor.Id)?.Hide ?? false;
-                    users = users.Where(u => !u.Hide || HideUser ).ToList(); // exclude self
+                    //bool HideUser = user?.FirstOrDefault(u => u.VendorId == vendor.Id)?.Hide ?? false;
+                   // users = users.Where(u => !u.Hide || HideUser ).ToList(); // exclude self
+                    users = users.Where(u => !u.Hide  ).ToList(); // exclude self
                     response.Body = JsonConvert.SerializeObject(users);
 
                 }
