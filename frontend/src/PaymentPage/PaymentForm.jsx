f9032bfa9d8dcf3c1a6c7a6c95f952b9dab0af8c
Rivka <instech101@gmail.com>
2026-03-09 14:27:08 -0400

    change policy to invoice

--- frontend/src/PaymentPage/PaymentForm.jsx
+++ frontend/src/PaymentPage/PaymentForm.jsx
@@ -554,7 +554,7 @@ export default function PaymentForm({ isPortal, onSuccess }) {
               <div >
                 <h3>
 
-                  Policy Details
+                  Invoice Details
                 </h3>
               </div>
               <div >
