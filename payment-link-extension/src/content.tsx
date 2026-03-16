f481964c3e787cbbf10711fc625081d1b41b6ca0
Rivka <instech101@gmail.com>
2026-03-16 10:04:38 -0400

    remove usestate -not necessary

--- payment-link-extension/src/content.tsx
+++ payment-link-extension/src/content.tsx
@@ -1,4 +1,4 @@
-import React ,{useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MainPage } from './components/MainPage';
 import { getAccountLookupCode } from './utils/api';
